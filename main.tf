41bc9caaabf9300e030b7cb90cb43e276e8d9def
AtifIrfan <74235725+AtifIrfan@users.noreply.github.com>
2025-05-23 17:08:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 resource "google_service_account" "default" {
   account_id   = "my-custom-sa"
   display_name = "Custom SA for VM Instance"
+  project = "seismic-kingdom-446107-g9"
 }
 
 resource "google_compute_instance" "confidential_instance" {
   name             = "my-confidential-instance"
+  project = "seismic-kingdom-446107-g9"
   zone             = "us-central1-a"
   machine_type     = "n2d-standard-2"
   min_cpu_platform = "AMD Milan"
